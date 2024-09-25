86cfc835226d730b6e932cc7d080f3c4d110b5ac
Bryan <bryanlondres5@gmail.com>
2024-09-25 12:46:22 -0400

    NewsAPI helper function

--- stocksentry.py
+++ stocksentry.py
@@ -2,6 +2,8 @@ import requests
 
 ALPHA_VANTAGE_API_KEY = 'your_alpha_vantage_api_key'
 BASE_URL = 'https://www.alphavantage.co/query'
+NEWS_API_KEY = 'your_news_api_key'
+
 
 def get_stock_price(symbol):
     params = {
@@ -14,3 +16,9 @@ def get_stock_price(symbol):
     data = response.json()
     # Parse and return the latest stock price
     return data['Time Series (1min)']
+
+def get_market_news():
+    url = f'https://newsapi.org/v2/top-headlines?category=business&apiKey={NEWS_API_KEY}'
+    response = requests.get(url)
+    articles = response.json().get('articles', [])
+    return articles[:5]  # Return top 5 articles
